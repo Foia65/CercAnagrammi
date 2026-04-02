76a4bd77d289b8498f2300a2ec8d1587b004ace2
Francesco Foianesi <foiasoft@gmail.com>
2026-04-02 17:12:49 +0200

    Initial Commit

---
+++ CercAnagrammi/CercAnagrammiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CercAnagrammiApp.swift
+//  CercAnagrammi
+//
+//  Created by Francesco Foianesi on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CercAnagrammiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
